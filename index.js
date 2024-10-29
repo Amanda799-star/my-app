e7bd3488a3a82d66cd9c2dd3ca515f2939b093cf
Amanda799-star <amandandalatheone@gmail.com>
2024-10-29 13:00:48 +0200

    Added new html, css and js files for my weather app

---
+++ index.js
@@ -0,0 +1,36 @@
+
+    let day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let d =new Date();
+    let days=day[d.getDay()];
+    let hours= d.getHours();
+    let minutes= d.getMinutes();
+    let date=document.querySelector("#time");
+    date.innerHTML=`${days} ${hours}:${minutes}`;
+
+
+    function searchEngine(response){
+      let temp=Math.round(response.data.temperature.current);
+      let getCity=document.querySelector("h1")
+      let weather=document.querySelector("#number")
+      let humidity=response.data.temperature.humidity
+      let cold=document.querySelector("#humid");
+      getCity.innerHTML=response.data.city;
+      weather.innerHTML=`${temp}  ℃`;
+      cold.innerHTML=`${humidity}%`;
+    
+
+    }
+
+function myForm(event){
+    event.preventDefault();
+let searchForm=document.querySelector("#input")
+let city=searchForm.value
+
+
+
+let apiurl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=3adfat7038a3b8f55f4do5846b4c6a7d&units=metric`;
+ axios.get(apiurl).then(searchEngine);
+}
+
+let form =document.querySelector("#form");
+form.addEventListener("submit",myForm);
